f3d16c328eec8334fe7313afa08a9281747c246c
Andy Eka Mulya <andymulya13@gmail.com>
2024-04-06 13:40:36 +0700

    Memperbaiki tampilan design awan

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,20 @@ export default function App(){
       
       {/* Slide 1 */}
       <section className="h-cover relative overflow-clip flex flex-col justify-center items-center text-center gap-5 py-5 bg-gradient-to-t from-blue-300 to-white">
-        <img src={ imgSudut } className="absolute w-52 rotate-180 -inset-14" />
-        <img src={ imgSudut } className="absolute w-52 -rotate-90 -top-12 -right-16" />
-        
-        <div className="font-dancingScript font-bold text-5xl text-blue-900 w-[300px] px-20">
-          <h1 className="text-left">Andy</h1>
-          <h1 className="text-right">
-            <span className="text-2xl text-black">&</span>
-            {" "}Angell
-          </h1>
-        </div>
+        <div className="z-20 flex flex-col items-center">
+          <img src={ imgSudut } className="absolute w-52 rotate-180 -inset-14" />
+          <img src={ imgSudut } className="absolute w-52 -rotate-90 -top-12 -right-16" />
+          
+          <div className="font-dancingScript font-bold text-5xl text-blue-900 w-[300px] px-20">
+            <h1 className="text-left">Andy</h1>
+            <h1 className="text-right">
+              <span className="text-2xl text-black">&</span>
+              {" "}Angell
+            </h1>
+          </div>
 
-        <HeroImage img={ imgAva1 } bingkai={ heroPattern1 } style={"w-52 h-52 top-12 right-10"}/>
+          <HeroImage img={ imgAva1 } bingkai={ heroPattern1 } style={"w-52 h-52 top-12 right-10"}/>
 
-        <div className="z-20">
           <p className="font-dancingScript font-extrabold text-blue-800 text-3xl">04 Maret 2025</p>
           <p className="px-5 font-semibold text-blue-950">Tanpa mengurangi rasa hormat, kami mengundang Bapak/Ibu/Saudara/i untuk hadir di acara kami.</p>
         </div>
@@ -34,7 +34,7 @@ export default function App(){
 
       {/* Slide 2 */}
       <section className="h-cover relative bg-gradient-to-t from-white to-blue-300 py-10 px-5 flex flex-col items-center text-center gap-5 overflow-clip">
-        <img src={ tepianAwan } className="absolute -top-20 rotate-180 z-10" />
+        <img src={ tepianAwan } className="absolute -top-24 rotate-180 -scale-x-100 z-10" />
         
         <div className="z-20 flex flex-col items-center">
           <div className="font-dancingScript text-4xl font-extrabold text-blue-900">
