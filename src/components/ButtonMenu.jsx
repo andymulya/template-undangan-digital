0d2d84227cdc540377c29007e629c40202c15d56
Andy Eka Mulya <andymulya13@gmail.com>
2024-04-17 18:35:25 +0700

    Memperbaiki bug pada design menu

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -13,19 +13,19 @@ const ButtonMenu = () => {
     return (
         <div className="absolute right-36 z-20">
             <section className="fixed bottom-0">
-                <LinkMenu id={"#home"} isActive={ isActive } duration={"duration-700"}>
+                <LinkMenu id={"#home"} isActive={ isActive } duration={"duration-300"}>
                     <IconHome />
                 </LinkMenu>
                 
-                <LinkMenu id={"#calendar"} isActive={ isActive } duration={"duration-150"}>
+                <LinkMenu id={"#calendar"} isActive={ isActive } duration={"duration-500"}>
                     <IconCalendar />
                 </LinkMenu>
 
-                <LinkMenu id={"#map"} isActive={ isActive } duration={"duration-300"}>
+                <LinkMenu id={"#map"} isActive={ isActive } duration={"duration-700"}>
                     <IconMap />
                 </LinkMenu>
 
-                <button onClick={ handleClick } className="bg-blue-800 rounded-full w-20 h-20 m-7 flex justify-center items-center text-white">
+                <button onClick={ handleClick } className="bg-blue-800 rounded-full z-20 w-20 h-20 m-7 flex justify-center items-center text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 font-bold">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
--- src/components/LinkMenu.jsx
+++ src/components/LinkMenu.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 
 const LinkMenu = ({ children, id, isActive, duration }) => {
     return (
-        <a href={ id } className={`bg-blue-500 flex justify-center items-center w-12 h-12 mb-3 font-bold text-white rounded-full translate-x-11 translate-y-[155px] transition-transform hide ${isActive && `-translate-y-1 ${ duration } show`}`}>
+        <a href={ id } className={`bg-blue-500 flex justify-center items-center w-12 h-12 mb-3 font-bold text-white rounded-full translate-x-11 transition-all ${!isActive ? `translate-y-[90px] z-10 ${ duration } hide` : `-translate-y-1 ${ duration } show`}`}>
             { children }
         </a>
     )
